39e70146b5715e5dbc0138c99b326ba3f18a101a
mafuba <tulkidg@gmail.com>
2023-08-19 19:55:34 +0300

    fix: Move SpringConfig.java to config package

--- src/main/java/ru/nasrulaev/spring/config/SpringConfig.java
+++ src/main/java/ru/nasrulaev/spring/config/SpringConfig.java
@@ -1,4 +1,4 @@
-package ru.nasrulaev.spring;
+package ru.nasrulaev.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
