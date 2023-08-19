c379c4e6d0c902939165375e13b7b238b8e3592d
mafuba <tulkidg@gmail.com>
2023-08-19 21:02:57 +0300

    feat: Create person model

---
+++ src/main/java/ru/nasrulaev/spring/models/Person.java
@@ -0,0 +1,40 @@
+package ru.nasrulaev.spring.models;
+
+import jakarta.validation.constraints.*;
+
+public class Person {
+
+    @NotBlank(message = "This field must be filled")
+    @Pattern(regexp = "^[A-ZА-Яа-я][a-zа-я]+(?: [A-ZА-Я][a-zа-я]*){0,2}$")
+    private String fullName;
+
+    @NotNull(message = "This field must be filled")
+    @Min(value = 1900, message = "Year of birth must be at least 1900")
+    @Max(value = 2009, message = "Year of birth must be less than 2010")
+    private int birthYear;
+
+
+    public Person() {
+    }
+
+    public Person(String fullName, int birthDate) {
+        this.fullName = fullName;
+        this.birthYear = birthDate;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+}
