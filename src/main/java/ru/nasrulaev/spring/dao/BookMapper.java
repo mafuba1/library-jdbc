b23edf343ace0bb36c2e99b0acbdafcc4888316f
mafuba <tulkidg@gmail.com>
2023-08-20 17:36:01 +0300

    feat(dao): Create BookMapper

---
+++ src/main/java/ru/nasrulaev/spring/dao/BookMapper.java
@@ -0,0 +1,23 @@
+package ru.nasrulaev.spring.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.nasrulaev.spring.models.Book;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookMapper implements RowMapper<Book> {
+
+    @Override
+    public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Book book = new Book();
+
+        book.setId(rs.getInt("book_id"));
+        book.setAuthorName(rs.getString("author_name"));
+        book.setBookName(rs.getString("book_name"));
+        book.setPublicationYear(rs.getInt("publication_year"));
+        book.setHolderId(rs.getObject("holder_id", Integer.class));
+
+        return book;
+    }
+}
