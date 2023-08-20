77e9994902ec231412291e38b03b79a5fa9e7769
mafuba <tulkidg@gmail.com>
2023-08-20 19:39:21 +0300

    feat(validation): Create PersonValidator
    Check name for uniqueness

---
+++ src/main/java/ru/nasrulaev/spring/validators/PersonValidator.java
@@ -0,0 +1,33 @@
+package ru.nasrulaev.spring.validators;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.nasrulaev.spring.dao.PersonDAO;
+import ru.nasrulaev.spring.models.Person;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+
+        if (personDAO.show(person.getFullName()).isPresent()) {
+            errors.rejectValue("fullName", "", "Full name must be unique");
+        }
+    }
+}
